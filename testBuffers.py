32bdb0f0d7db070b82b91a13b576d22fde6f6a19
bresslem <bresslem@informatik.hu-berlin.de>
2021-01-13 14:35:39 +0100

    tutorial

--- testBuffers.py
+++ testBuffers.py
@@ -9,7 +9,7 @@ from OpenGL.arrays import vbo
 import numpy
 import sys
 
-vertexShaderSource = """#version 460 core
+vertexShaderSource = """#version 330 core
 layout (location = 0) in vec3 aPos;
 
 void main()
@@ -18,7 +18,7 @@ void main()
 } """
 
 
-fragmentShaderSource = """#version 150
+fragmentShaderSource = """#version 330 core
 out vec4 out_color;
 void main()
 {
@@ -26,54 +26,44 @@ void main()
 }"""
 
 
-X = 0.52573111212
-Z = 0.85065080835
-
-vertices = numpy.array(
-    [-X, 0, Z,
-    X, 0, Z,
-    -X, 0, -Z,
-    X, 0, -Z,
-    0, Z, X,
-    0, Z, -X,
-    0, -Z, X,
-    0, -Z, -X,
-    Z, X, 0,
-    -Z, X, 0,
-    Z, -X, 0,
-    -Z, -X, 0], dtype='float32')
-
-triangles = numpy.array(
-    [0,4,1,
-     0,9,4,
-     9,5,4,
-     4,5,8,
-     4,8,1,
-     8,10,1,
-     8,3,10,
-     5,3,8,
-     5,2,3,
-     2,7,3,
-     7,10,3,
-     7,6,10,
-     7,11,6,
-     11,0,6,
-     0,1,6,
-     6,1,10,
-     9,0,11,
-     9,11,2,
-     9,2,5,
-     7,2,11], dtype='int16')
-
-
-def icosahedron():
-    vertices_r = vertices.reshape((12, 3))
-    triangles_r = triangles.reshape((20, 3))
-    glBegin(GL_TRIANGLES)
-    for triangle in triangles_r:
-        for vertex in triangle:
-            glVertex3fv(vertices_r[vertex])
-    glEnd()
+# X = 0.52573111212
+# Z = 0.85065080835
+#
+# vertices = numpy.array(
+#     [-X, 0, Z,
+#     X, 0, Z,
+#     -X, 0, -Z,
+#     X, 0, -Z,
+#     0, Z, X,
+#     0, Z, -X,
+#     0, -Z, X,
+#     0, -Z, -X,
+#     Z, X, 0,
+#     -Z, X, 0,
+#     Z, -X, 0,
+#     -Z, -X, 0], dtype='float32')
+#
+# triangles = numpy.array(
+#     [0,4,1,
+#      0,9,4,
+#      9,5,4,
+#      4,5,8,
+#      4,8,1,
+#      8,10,1,
+#      8,3,10,
+#      5,3,8,
+#      5,2,3,
+#      2,7,3,
+#      7,10,3,
+#      7,6,10,
+#      7,11,6,
+#      11,0,6,
+#      0,1,6,
+#      6,1,10,
+#      9,0,11,
+#      9,11,2,
+#      9,2,5,
+#      7,2,11], dtype='int16')
 
 def main():
     pg.init()
@@ -82,24 +72,9 @@ def main():
     pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 0)
     pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE)
     pg.display.set_mode(display, DOUBLEBUF|OPENGL)
-    print(glGetString(GL_VERSION))
+    # print(glGetString(GL_VERSION))
 
-    # gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
-
-    VAO = glGenVertexArrays(1)
-    glBindVertexArray(VAO)
-
-    VBO = glGenBuffers(1)
-    glBindBuffer(GL_ARRAY_BUFFER, VBO)
-    glBufferData(GL_ARRAY_BUFFER, vertices, GL_DYNAMIC_DRAW)
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, None)
-    glEnableVertexAttribArray(0)
-    glBindVertexArray(0)
-
-    EBO = glGenBuffers(1)
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO)
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, triangles, GL_DYNAMIC_DRAW)
-
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
 
     vertexShader = glCreateShader(GL_VERTEX_SHADER)
     glShaderSource(vertexShader, vertexShaderSource)
@@ -114,24 +89,51 @@ def main():
     glAttachShader(shaderProgram, fragmentShader)
     glLinkProgram(shaderProgram)
 
+    glDeleteShader(vertexShader)
+    glDeleteShader(fragmentShader)
+
+    # vertices = numpy.array([
+    #      0.5,  0.5, 0.0,
+    #      0.5, -0.5, 0.0,
+    #     -0.5, -0.5, 0.0,
+    #     -0.5,  0.5, 0.0
+    # ], dtype = 'float32')
+    #
+    # triangles = numpy.array([
+    #     0, 1, 3,
+    #     1, 2, 3
+    # ], dtype='int16')
+
+    VAO = glGenVertexArrays(1)
+    VBO = glGenBuffers(1)
+    EBO = glGenBuffers(1)
+
+    glBindVertexArray(VAO)
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO)
+    glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW)
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO)
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, triangles, GL_STATIC_DRAW)
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, None)
+    glEnableVertexAttribArray(0)
+    glBindBuffer(GL_ARRAY_BUFFER, 0)
+    glBindVertexArray(0)
+
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 quit()
-
-        glRotatef(1, 1, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         glUseProgram(shaderProgram)
         glBindVertexArray(VAO)
-        glDrawArrays(GL_TRIANGLES, 0, 3)
-        # glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0)
+        # glDrawArrays(GL_TRIANGLES, 0, 3)
+        glDrawElements(GL_TRIANGLES, 60, GL_UNSIGNED_INT, 0)
         pg.display.flip()
         pg.time.wait(10)
-
-    glDeleteShader(vertexShader)
-    glDeleteShader(fragmentShader)
 
 if __name__ == "__main__":
     main()
